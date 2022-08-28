7c492a0b8a4334ce6b9d961eb97e3445c7436f0a
Makne shilpa <shilpamakne62@gmail.com>
2022-08-28 09:58:08 +0000

    Registerr

---
+++ WishList/Models/AccountViewModel/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models.AccountViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100,MinimumLength =8)]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("password")]
+        public string confirmpassword { get; set; }
+
+    }
+}
