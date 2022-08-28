cfedb367a5168ebaa8e02debbeb85ff56577bc1c
Makne shilpa <shilpamakne62@gmail.com>
2022-08-28 10:31:07 +0000

    http

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -32,6 +32,17 @@ namespace WishList.Controllers
         [AllowAnonymous]
         public IActionResult Register(RegisterViewModel model)
         {
+            if (!ModelState.IsValid)
+                return View(model);
+           var result= _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.password).Result;
+           if(!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("password", error.Description);
+                }
+                return View(model);
+            }
             return RedirectToAction("Index", "Home");
 
         }
