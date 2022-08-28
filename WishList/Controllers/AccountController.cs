4531eba877b9ad0f084f52968eb44b0913e4d2e4
Makne shilpa <shilpamakne62@gmail.com>
2022-08-28 07:05:53 +0000

    Manager

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
     }
 }
