cc9b0ca1e9e6169f3028bb5fdeea8359cc46b044
Makne shilpa <shilpamakne62@gmail.com>
2022-08-29 05:25:06 +0000

    Logout

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -68,6 +68,13 @@ namespace WishList.Controllers
             }
             return RedirectToAction("Index", "Item");
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
         
     }
 }
